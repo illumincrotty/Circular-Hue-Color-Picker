21bed23d899412ac2338d7a439a4a6dafbdac802
Brian Crotty <5673408+Brian-Crotty@users.noreply.github.com>
2021-05-12 22:27:08 -0500

    Moved addition of stylesheet to html file

--- src/tsAttempt2/index.ts
+++ src/tsAttempt2/index.ts
@@ -2,14 +2,6 @@ import { colorPickerComponent } from './component.js';
 const colorPicker = document.getElementById('colorPicker-app');
 
 if (colorPicker) {
-	const linkElem = document.createElement('link');
-	linkElem.setAttribute('rel', 'stylesheet');
-	linkElem.setAttribute(
-		'href',
-		'../../style/tsAttempt2/colorPickerStyle.css'
-	);
-	linkElem.id = 'colorPicker-style';
-	document.head.appendChild(linkElem);
 	const component = new colorPickerComponent(colorPicker);
 	component.resize();
 }
